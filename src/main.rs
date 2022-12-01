9923e91ecfc81e9b5297d4acde348b87e8a4204d
异月 <twhice@outlook.com>
2022-12-01 16:02:48 +0800

    重命名仓库&编写文档

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::{fmt::Error, process::ExitCode};
 
 fn main() -> Result<ExitCode, Error> {
     if false {
-        mindustry_rogic_hardening::old_err_test();
-        mindustry_rogic_hardening::old_lexer_test();
-        mindustry_rogic_hardening::old_repl();
-        mindustry_rogic_hardening::old_parser_test();
+        mindustry_logic_language::old_err_test();
+        mindustry_logic_language::old_lexer_test();
+        mindustry_logic_language::old_repl();
+        mindustry_logic_language::old_parser_test();
     }
     let mut src = String::new();
     src += "if 1 {\n";
@@ -18,6 +18,6 @@ fn main() -> Result<ExitCode, Error> {
     src += "}else{\n";
     src += "    set a = 1\n";
     src += "}\n";
-    mindustry_rogic_hardening::run(src, "main.mrh");
+    mindustry_logic_language::run(src, "main.mrh");
     return Ok(ExitCode::SUCCESS);
 }
